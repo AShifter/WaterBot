8fec277b1d4dff51f1095a24655b0e825e9ad565
AShifter <loganr.lowe@gmail.com>
2018-03-01 11:33:50 -0700

    add OS package and update about command
    yay

--- modules/commands/about.js
+++ modules/commands/about.js
@@ -2,7 +2,10 @@ module.exports.run = async (client, message, args, bot) =>
 {
     const conf = require("../config.json");
     const Discord = require('discord.js');
+    const os = require('os');
 
+    var mem = os.totalmem() / 1000000;
+    var memfree = os.freemem() / 1000000;
     const embed = new Discord.RichEmbed()
     .setColor(conf.embedColor)
     .setTitle(`${conf.name} ${conf.version} | About`)
@@ -11,7 +14,11 @@ module.exports.run = async (client, message, args, bot) =>
             `${conf.name}'s command handler was written by Logan Lowe (AShifter#6159). Many of the modules used in WaterBot were written by Richard Moch (FloppyDiskDrive#2282) under the MIT license. It can be found here: https://github.com/jtsshieh/precipitation-js`)
     .addField("License",
             `${conf.name} is an open-source Discord moderation bot - it's components are licensed under the MIT license. Basically, do whatever you want with it as long as you give credit back. Simple, right? https://github.com/AShifter/WaterBot`)
-    message.channel.send({ embed });
+    .addField("Software",
+            `${conf.name} is currently running on ${os.type} ${os.arch} and NodeJS ${process.version}.`)
+    .addField("Hardware",
+            `${conf.name} is currently running on this hardware:\n${os.cpus()[1].model}\n${Math.round(mem)}MB RAM (${Math.round(memfree)}MB free)\n`)
+            message.channel.send({ embed });
 }
 
 module.exports.help = {
